14803d8bcbe6338a8c2bf0211c08c7f89b28d4b9
nosrednawall <nosrednawall@gmail.com>
2025-01-07 09:44:06 -0300

    ajuste

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -3,6 +3,7 @@
 # Carrega configuracoes do tema
 source /home/anderson/.theme_selected
 source /home/anderson/.bashrc
+xrdb -merge ~/.Xresources
 
 # Executa o script para verificar se o segundo monitor está ativo
 if [ ! -f ~/.monitor_config.sh ]
@@ -12,24 +13,16 @@ else
     bash ~/.monitor_config.sh
 fi
 
-xdotool key control+alt+d
-
 # Define o papel de parede usando feh
 feh --recursive --bg-fill --randomize ~/.wallpapers/${THEME_MODE}/${COLOR_MODE}/ &
+
+# Abre o dwmblocks
+xdotool key control+alt+d
 
 # Função para verificar se um processo está rodando
 is_running() {
     ps aux | grep -v grep | grep -q "$1"
 }
-
-# Inicia o mate-power-manager se não estiver rodando
-# is_running "xfce4-power-manager" || xfce4-power-manager &
-
-# Applet som
-#is_running "pasystray" || pasystray &
-
-# Wifi
-#is_running "nm-applet" || nm-applet &
 
 # Polkit - para autenticar programas
 is_running "lxpolkit" || lxpolkit &
@@ -55,5 +48,14 @@ is_running "solaar" || /usr/bin/solaar -w hide
 #killall dwmblocks; export PATH=$PATH:$HOME/.config/suckless/scripts/dwmblocks && $HOME/.config/suckless/dwmblocks-async/build/dwmblocks &
 #is_running "dwmblocks" || /usr/local/bin/dwmblocks &
 
+
+# Inicia o mate-power-manager se não estiver rodando
+# is_running "xfce4-power-manager" || xfce4-power-manager &
+
+# Applet som
+#is_running "pasystray" || pasystray &
+
+# Wifi
+#is_running "nm-applet" || nm-applet &
 
 #bash ~/.local/bin/roda_dwmblocks.sh
